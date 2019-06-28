970f6b453ffb8c282d5ebd6bf8931aa943d70010
Chad <chad.m.carlson@gmail.com>
2019-06-28 14:23:36 -0600

    refactored handleSetPrice

--- client/src/components/AndroidDisplay.js
+++ client/src/components/AndroidDisplay.js
@@ -24,7 +24,7 @@ const WebDisplay = () => {
             {/* <Features catID={c.id}/> */}
           </Container>
           )}
-          <OSMath OS='Android' />
+          <OSMath OS='android' />
       </ul>
     </>
   );
