3e4600fb7389569f161916ce1b95a4cf62ea4e99
Chad <chad.m.carlson@gmail.com>
2019-07-01 16:06:27 -0600

    features form displaying categories with correct features

--- client/src/components/AndroidDisplay.js
+++ client/src/components/AndroidDisplay.js
@@ -1,36 +1,94 @@
-import React, {useState, useEffect, } from 'react';
+import React, {useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import OSMath from './OSMath';
-// import Features from './Features';
-import {Container, Segment } from 'semantic-ui-react';
-import TotalMath from "./TotalMath";
+import {Form, Container, Header,} from 'semantic-ui-react';
+// import { AuthContext } from '../providers/AuthProvider';
+import { MathContext} from '../providers/MathProvider';
 
-
-const WebDisplay = () => {
+const Features = (props) => {
   const [categories, setCategories] = useState([]);
+  const [features, setFeatures] = useState([]);
+  const [value, setValue] = useState('');
+  // const auth = useContext(AuthContext)
+  const { handleSetPrice } = useContext(MathContext)
 
   useEffect( () => {
-    axios.get(`/api/categories`,{params: {os: 'android'}} )
-      .then( res => {
-        setCategories(res.data) 
-      });
-  },[]);
+      axios.get(`/api/categories`,{params: {os: 'android'}})
+    .then( res  => {
+      setCategories(res.data)
+      console.table(res.data)
+    })
+    axios.get(`/api/features`)
+      .then(res => setFeatures(res.data))
+  },[])
+
+  const handleChange = (e) => {
+
+    setValue(e.value)}
+
+  const exclusiveRendering = (catID, is_exclusive) => {
+    let correctF = features.filter( f => catID === f.category_id);
+    console.log(correctF)
+
+    if (is_exclusive === true) {
+      return (
+        <>
+              {correctF.map( f => (
+        <Form.Group>
+                <Form.Radio
+                  name={f.name}
+                  checked={value === f.id}
+                  value={f.id}
+                  label={f.description}
+                  onChange={handleChange}
+                  />
+          </Form.Group>
+            ))}
+            </>
+        )
+      }
+      else {
+        return (
+          <Form.Group>
+            {correctF.map( f => (
+                <Form.Input
+                  type='checkbox'
+                  name={f.name}
+                  label={f.description}
+                  />
+            ))}
+          </Form.Group>
+        )
+      }
+    };
+  
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    // //////
+    // .then( res => {
+    //   props.add(res.data);
+    // })
+    handleSetPrice(props.OS)
+  }
 
   return (
     <>
       <ul>
-        <h1>Android</h1>
-        {categories.map(c => 
+        <Form onSubmit={handleSubmit()}>
+          {categories.map(c => 
           <Container key={c.id} id={c.id}>
-            <Segment>{c.name}</Segment>
-            {/* <Features catID={c.id} exclusivity={c.is_exclusive}/> */}
+            <Header as="h1">{c.name}</Header>
+            {/* <exclusiveRendering  
+              catID={c.id} 
+              isExclusive={c.is_exclusive} /> */}
+            {exclusiveRendering(c.id, c.is_exclusive)}
           </Container>
           )}
-          <OSMath OS='android' />
-          < TotalMath />
+          <Form.Button>Submit for Quote</Form.Button>
+        </Form >
       </ul>
     </>
-  );
+  )
 };
 
-export default WebDisplay;
+export default Features;
--- client/src/components/Features.js
+++ client/src/components/Features.js
@@ -1,60 +1,66 @@
 import React, {useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import {Form, Container, Header, Item } from 'semantic-ui-react';
+import {Form, Container, Header,} from 'semantic-ui-react';
 // import { AuthContext } from '../providers/AuthProvider';
 import { MathContext} from '../providers/MathProvider';
 
 const Features = (props) => {
   const [categories, setCategories] = useState([]);
   const [features, setFeatures] = useState([]);
+  const [value, setValue] = useState('');
   // const auth = useContext(AuthContext)
   const { handleSetPrice } = useContext(MathContext)
 
   useEffect( () => {
-    axios.all([
-      axios.get(`/api/categories`,{params: {os: 'android'}} ),
-      axios.get(`/api/features`,{params: {category_id: 1 }})
-    ])
-    .then(axios.spread((categoryRes, featureRes) => {
-      setCategories(categoryRes.data)
-      setFeatures(featureRes.data)
-    }))
+      axios.get(`/api/categories`,{params: {os: 'web'}})
+    .then( res  => {
+      setCategories(res.data)
+      console.table(res.data)
+    })
+    axios.get(`/api/features`)
+      .then(res => setFeatures(res.data))
   },[])
 
-  const ExclusiveRendering = (props) => {
-    const CorrectFeature = (props) => {
-      var correctF = features.filter( f => 
-        props.catID === f.category_id,
-        // correctF.push(f)
-      )
-      return (correctF)
-    }
+  const handleChange = (e) => {
 
-    CorrectFeature.map(f => {
-      if (props.isExclusive) {
-        return (
-          f.map(a => (
-          <Form.Group>
-            <Form.Input
-            type="radio"
-            name={a.name}
-            />
-            <Item.description>{a.description}</Item.description>
-          </Form.Group>))
-        );
-      }
+    setValue(e.value)}
+
+  const exclusiveRendering = (catID, is_exclusive) => {
+    let correctF = features.filter( f => catID === f.category_id);
+    console.log(correctF)
+
+    if (is_exclusive === true) {
       return (
-        f.map(a => (
+        <>
+              {correctF.map( f => (
         <Form.Group>
-          <Form.Input
-          type="checkbox"
-          name={a.name}
-          />
-          <Item.description>{a.description}</Item.description>
-        </Form.Group>))
-      );
-    });
-  };
+                <Form.Radio
+                  name={f.name}
+                  checked={value === f.id}
+                  value={f.id}
+                  label={f.description}
+                  onChange={handleChange}
+                  />
+          </Form.Group>
+            ))}
+            </>
+        )
+      }
+      else {
+        return (
+          <Form.Group>
+            {correctF.map( f => (
+                <Form.Input
+                  type='checkbox'
+                  name={f.name}
+                  label={f.description}
+                  />
+            ))}
+          </Form.Group>
+        )
+      }
+    };
+  
 
   const handleSubmit = (e) => {
     // e.preventDefault();
@@ -72,7 +78,10 @@ const Features = (props) => {
           {categories.map(c => 
           <Container key={c.id} id={c.id}>
             <Header as="h1">{c.name}</Header>
-            <ExclusiveRendering  catID={c.id} isExclusive={c.is_exclusive} />
+            {/* <exclusiveRendering  
+              catID={c.id} 
+              isExclusive={c.is_exclusive} /> */}
+            {exclusiveRendering(c.id, c.is_exclusive)}
           </Container>
           )}
           <Form.Button>Submit for Quote</Form.Button>
