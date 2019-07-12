6fea6944ffba7adacc85e748962c2aa5811151eb
Chad Carlson <44014349+chad-m-carlson@users.noreply.github.com>
2019-07-12 09:18:22 -0600

    Update 20190712065651_add_column_to_estimates.rb
    Changed design type from integer to float

--- db/migrate/20190712065651_add_column_to_estimates.rb
+++ db/migrate/20190712065651_add_column_to_estimates.rb
@@ -1,6 +1,6 @@
 class AddColumnToEstimates < ActiveRecord::Migration[5.2]
   def change
-    add_column :estimates, :design, :integer
+    add_column :estimates, :design, :float
     add_column :estimates, :qaTesting, :float
     add_column :estimates, :deployment, :float
     add_column :estimates, :postDeploymentDev, :float
